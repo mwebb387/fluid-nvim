b9c61459055bb86bdd9a438b93100cf239a4ba41
Mike Webb <mwebb@bizstream.com>
2023-06-01 10:22:50 -0400

    Added commands for using with asyncrun

--- lua/fluid/modules/asyncrun/init.lua
+++ lua/fluid/modules/asyncrun/init.lua
@@ -1,7 +1,23 @@
 local M = {}
 
 function M:init()
-  self:use('skywind3000/asyncrun.vim')
+  self
+    :use('skywind3000/asyncrun.vim')
+    :depends_on('fluid.nvim').as('nvim')
+end
+
+function M:setup(deps)
+  -- vim.g.asyncrun_open = true
+  deps.nvim:command('Rg', function(input)
+    vim.cmd('AsyncRun ' .. vim.o.grepprg .. ' ' .. input.args .. ' .')
+    vim.cmd.copen()
+  end, {nargs = 1})
+
+
+  deps.nvim:command('Mk', function(input)
+    vim.cmd ("AsyncRun " .. vim.o.makeprg .. ' ' .. input.args .. ' .')
+    vim.cmd.copen()
+  end, {nargs = "?"})
 end
 
 return M
