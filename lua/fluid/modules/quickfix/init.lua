bbe9246ff330b64f88f6344855c843272dc269d6
Michael Webb <mike.d.webb@gmail.com>
2024-10-29 10:18:37 -0400

    Added QuickFix list toggle

--- lua/fluid/modules/quickfix/init.lua
+++ lua/fluid/modules/quickfix/init.lua
@@ -6,7 +6,23 @@ function M:init()
     :use('kevinhwang91/nvim-bqf')
 end
 
--- function M:setup(deps)
--- end
+function M:setup()
+  -- Quick Fix List toggle
+  vim.keymap.set('n', '<leader>q', function()
+    local qf_open = false
+    for winnr = 1, vim.fn.winnr('$') do
+      if vim.fn.getwinvar(winnr, '&syntax') == 'qf' then
+        qf_open = true
+      end
+    end
+
+    if qf_open then
+      vim.cmd.cclose()
+    else
+      vim.cmd.copen()
+    end
+  end)
+
+end
 
 return M
