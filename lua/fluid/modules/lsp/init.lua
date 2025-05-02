17f2996ebf6177f70a25d1af977f80b6b43f21c9
Michael Webb <mike.d.webb@gmail.com>
2025-05-02 09:09:15 -0400

    Updated deprecated code for diagnostic signs

--- lua/fluid/modules/lsp/init.lua
+++ lua/fluid/modules/lsp/init.lua
@@ -84,10 +84,16 @@ end
 
 function M:setup(deps)
   if self:has('icons') then
-    vim.fn.sign_define('DiagnosticSignError', {text = '', texthl = 'DiagnosticSignError'})
-    vim.fn.sign_define('DiagnosticSignHint', {text = '', texthl = 'DiagnosticSignHint'})
-    vim.fn.sign_define('DiagnosticSignInfo', {text = '', texthl = 'DiagnosticSignInfo'})
-    vim.fn.sign_define('DiagnosticSignWarn', {text = '', texthl = 'DiagnosticSignWarn'})
+    vim.diagnostic.config({
+      signs = {
+        text = {
+          [vim.diagnostic.severity.ERROR] = '',
+          [vim.diagnostic.severity.WARN] = '',
+          [vim.diagnostic.severity.INFO] = '',
+          [vim.diagnostic.severity.HINT] = '',
+        }
+      }
+    })
   end
 
   if self:has('server_management') then
